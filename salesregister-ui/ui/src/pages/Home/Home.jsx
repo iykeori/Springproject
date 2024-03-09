7ed6c7a7aa2c2af61436018d801c7449e0365ca9
iykeori <oricletus@gmail.com>
2024-03-09 14:41:09 -0400

    updates

--- salesregister-ui/ui/src/pages/Home/Home.jsx
+++ salesregister-ui/ui/src/pages/Home/Home.jsx
@@ -6,14 +6,14 @@ import {useNavigate} from "react-router-dom";
 const Home = ({props}) => {
 
     const navigate = useNavigate();
-    const handleRegisterClick = () => {
-        navigate("/register");
-    };
+    // const handleRegisterClick = () => {
+    //     navigate("/register");
+    // };
 
     return (
         <div className={styles.main}>
             {/* <h1>Welcome to Transaction Register</h1> */}
-            <button onClick={handleRegisterClick}>Register Product</button>
+        <button onClick={() => navigate("/register")}>Register Product</button>
 
         </div>
     );
